67d38d859efe0c3d57fc53289837e59ccd8090f3
euler <1623190186@qq.com>
2025-12-11 17:51:40 +0800

    fix:测试部署

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,8 @@
+import { host } from "@/config";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  baseURL: host,
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
