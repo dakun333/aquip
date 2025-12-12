cab353b79c04afab324550612c735d0136979444
euler <1623190186@qq.com>
2025-12-12 12:37:29 +0800

    fix:测试

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,11 +1,17 @@
-import { host } from "@/config";
 import { createAuthClient } from "better-auth/react";
-
+const baseURL: string | undefined =
+  process.env.VERCEL === "1"
+    ? process.env.VERCEL_ENV === "production"
+      ? process.env.BETTER_AUTH_URL
+      : process.env.VERCEL_ENV === "preview"
+      ? `https://${process.env.VERCEL_URL}`
+      : undefined
+    : undefined;
 export const authClient = createAuthClient({
-  baseURL: host,
+  baseURL,
 });
-console.log("authClient", authClient);
-console.log("host", host);
+console.log("baseURL", baseURL);
+
 console.log("VERCEL_URL", process.env.VERCEL_URL);
 
 export const { signIn, signOut, signUp, useSession } = authClient;
