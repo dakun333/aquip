69a687b4b92695f8c7f49dfce71fe2132ec9ed24
euler <1623190186@qq.com>
2025-12-12 11:37:28 +0800

    feat:修改渲染界面

--- app/[locale]/(main)/my/page.tsx
+++ app/[locale]/(main)/my/page.tsx
@@ -1,34 +1,16 @@
+"use client";
 import { Metadata } from "next";
-
-import { getTranslations, setRequestLocale } from "next-intl/server";
 import Link from "next/link";
 
 import { AQButton } from "../../ui/button";
-import { Locale } from "next-intl";
-import { auth } from "@/lib/auth";
+import { Locale, useTranslations } from "next-intl";
+
 import { useSession } from "@/lib/auth-client";
-import { headers } from "next/headers";
-export async function generateMetadata(
-  props: Omit<PageProps<"/[locale]/my">, "children">
-) {
-  const { locale } = await props.params;
 
-  const t = await getTranslations({
-    locale: locale as Locale,
-    namespace: "my",
-  });
+export default function My({ params }: PageProps<"/[locale]/my">) {
+  const t = useTranslations("my");
+  const res = useSession();
 
-  return {
-    title: t("title"),
-  };
-}
-export default async function My({ params }: PageProps<"/[locale]/my">) {
-  const { locale } = await params;
-  setRequestLocale(locale as Locale);
-  const t = await getTranslations("my");
-  const res = await auth.api.getSession({
-    headers: await headers(), // you need to pass the headers object.
-  });
   return (
     <>
       <div className="h-full w-full flex flex-col">
@@ -36,9 +18,9 @@ export default async function My({ params }: PageProps<"/[locale]/my">) {
           {t("title")}
         </div>
         <div className="flex-1 overflow-y-auto flex flex-col justify-center items-center gap-2">
-          <div>当前用户名：{res?.user.id}</div>
-          <div>当前用户名：{res?.user.name}</div>
-          <div>当前用户名：{res?.user.email}</div>
+          <div>当前用户名：{res?.data?.user.id}</div>
+          <div>当前用户名：{res?.data?.user.name}</div>
+          <div>当前用户名：{res?.data?.user.email}</div>
 
           <Link href="/sign-up">
             <AQButton>注册</AQButton>
